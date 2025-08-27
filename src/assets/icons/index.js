5e4e42f62b2d6a322ff1911213c5c0d9ce0bffb0
a_ovsyannikov <a_ovsyannikov@net.gsl.org>
2025-08-27 23:30:36 +0300

    add new lines

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,31 +1,11 @@
 import ArrowBack from './arrow-back.svg?raw';
-import Attachment from './attachment.svg?raw';
 import ChatAdd from './chat-add.svg?raw';
-import Done from './done.svg?raw';
-import Edit from './edit.svg?raw';
 import Logout from './logout.svg?raw';
-import More from './more.svg?raw';
-import Picture from './picture.svg?raw';
-import Send from './send.svg?raw';
 import Settings from './settings.svg?raw';
-import Trash from './trash.svg?raw';
-import UserAdd from './user-add.svg?raw';
-import UserDelete from './user-delete.svg?raw';
-import User from './user.svg?raw';
 
 export {
   ArrowBack,
-  Attachment,
   ChatAdd,
-  Done,
-  Edit,
   Logout,
-  More,
-  Picture,
-  Send,
   Settings,
-  Trash,
-  UserAdd,
-  UserDelete,
-  User
 };
